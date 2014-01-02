1449f1ebe0b12f296c0698a6f565840943a9cc69
Krystle Bates <krebates@gmail.com>
2014-01-02 14:48:17 -0500

    committed work v2

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -2,6 +2,7 @@ class MenuItemsController < ApplicationController
   helper_method :menu_items
 
   def index
+    @menu_items = MenuItem.all
   end
 
   def show
@@ -10,17 +11,29 @@ class MenuItemsController < ApplicationController
 
   def new
     @menu_item = MenuItem.new
+    @menu_items = MenuItem.find(:all)
   end
 
   def create
     @menu_item = MenuItem.new(menu_item_params)
 
     if @menu_item.save
-      redirect_to menu_item_path(@menu_item),
+      redirect_to menu_items_path,
         notice: "Menu item created successfully!"
     else
       render :new
     end
+  end
+
+  def destroy
+    @menu_item = MenuItem.find_by_id(params[:id])
+      if @menu_item.destroy
+        redirect_to menu_items_path
+      end
+  end
+
+  def edit
+    # @menu_item = MenuItem.find(params[:id])
   end
 
   private
@@ -28,7 +41,7 @@ class MenuItemsController < ApplicationController
     @menu_items ||= MenuItem.all
   end
 
-  def menu_item_params 
-    params.require(:menu_item).permit(:name, :description, :price_in_cents)
+  def menu_item_params
+    params.require(:menu_item).permit(:id, :name, :description, :price_in_cents)
   end
 end
